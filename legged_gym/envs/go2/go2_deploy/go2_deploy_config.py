b2636c3fe664b98f3b25812e3b8b3374de685657
lupinjia <jiayasen021003@gmail.com>
2025-08-14 10:16:59 +0800

    modify go2_deploy to "walk these ways" style

--- legged_gym/envs/go2/go2_deploy/go2_deploy_config.py
+++ legged_gym/envs/go2/go2_deploy/go2_deploy_config.py
@@ -10,9 +10,9 @@ class GO2DeployCfg(LeggedRobotCfg):
         # observation history
         frame_stack = 5   # policy frame stack
         c_frame_stack = 3  # critic frame stack
-        num_single_obs = 55
+        num_single_obs = 52
         num_observations = int(num_single_obs * frame_stack)
-        single_num_privileged_obs = 73 + 27
+        single_num_privileged_obs = 73 + 24
         num_privileged_obs = int(c_frame_stack * single_num_privileged_obs)
 
     class terrain(LeggedRobotCfg.terrain):
@@ -73,9 +73,11 @@ class GO2DeployCfg(LeggedRobotCfg):
 
     class rewards(LeggedRobotCfg.rewards):
         soft_dof_pos_limit = 0.9
-        base_height_target = 0.34
         
-        foot_clearance_target = 0.06  # desired foot clearance above ground [m]
+        base_height_target_range = [0.2, 0.35]
+        base_height_tracking_sigma = 0.01
+        
+        foot_clearance_target_range = [0.03, 0.1]  # desired foot clearance above ground [m]
         foot_height_offset = 0.022 # height of the foot coordinate origin above ground [m]
         foot_clearance_tracking_sigma = 0.01
         
@@ -90,9 +92,11 @@ class GO2DeployCfg(LeggedRobotCfg):
             # command tracking
             tracking_lin_vel = 1.0
             tracking_ang_vel = 0.5
+            tracking_base_height = 1.0
+            foot_clearance = 1.0
+            quad_periodic_gait = 1.0
             # smooth
             lin_vel_z = -0.5
-            base_height = -1.0
             ang_vel_xy = -0.05
             orientation = -2.0
             dof_vel = -5.e-4
@@ -101,10 +105,8 @@ class GO2DeployCfg(LeggedRobotCfg):
             action_smoothness = -0.01
             torques = -2.e-4
             foot_landing_vel = -0.15
-            # gait
-            quad_periodic_gait = 1.0
-            foot_clearance = 0.5
-
+            hip_pos = -1.0
+            
         class periodic_reward_framework:
             '''Periodic reward framework in OSU's paper(https://arxiv.org/abs/2011.01387)'''
             gait_function_type = "step" # can be "step" or "smooth"
@@ -112,13 +114,11 @@ class GO2DeployCfg(LeggedRobotCfg):
             resampling_time = 6.0                       # gait resampling time [s]
             gait_period_range = [0.3, 0.6]                         # gait period [s]
             # start of swing is all the same
-            b_swing = [0.5]
-            theta_fl = [0.0]  # front left leg
-            theta_fr = [0.5]
-            theta_rl = [0.5]  # rear left leg
-            theta_rr = [0.0]
-            swing_phase_ratio = b_swing
-            stance_phase_ratio = [1 - swing_phase_ratio[0]]
+            b_swing = 0.5
+            theta_fl = 0.0  # front left leg
+            theta_fr = 0.5
+            theta_rl = 0.5
+            theta_rr = 0.0
 
     class commands(LeggedRobotCfg.commands):
         curriculum = True
@@ -135,25 +135,26 @@ class GO2DeployCfg(LeggedRobotCfg):
             heading = [-3.14, 3.14]
 
     class domain_rand(LeggedRobotCfg.domain_rand):
-        randomize_friction = True
+        enable = True
+        randomize_friction = enable
         friction_range = [0.2, 1.7]
-        randomize_base_mass = True
+        randomize_base_mass = enable
         added_mass_range = [-1., 1.]
-        push_robots = True
+        push_robots = enable
         push_interval_s = 15
         max_push_vel_xy = 1.
-        randomize_com_displacement = True
+        randomize_com_displacement = enable
         com_displacement_range = [-0.03, 0.03]
         randomize_ctrl_delay = False
         ctrl_delay_step_range = [0, 1]
-        randomize_pd_gain = True
+        randomize_pd_gain = enable
         kp_range = [0.8, 1.2]
         kd_range = [0.8, 1.2]
-        randomize_joint_armature = True
+        randomize_joint_armature = enable
         joint_armature_range = [0.015, 0.025]  # [N*m*s/rad]
-        randomize_joint_stiffness = True
+        randomize_joint_stiffness = enable
         joint_stiffness_range = [0.01, 0.02]
-        randomize_joint_damping = True
+        randomize_joint_damping = enable
         joint_damping_range = [0.25, 0.3]
 
     class normalization(LeggedRobotCfg.normalization):
@@ -180,6 +181,6 @@ class GO2DeployCfgPPO(LeggedRobotCfgPPO):
         run_name = 'step_gait'
         experiment_name = 'go2_deploy'
         save_interval = 100
-        load_run = "Aug12_15-47-58_step_gait"
+        load_run = "Aug13_18-24-58_step_gait"
         checkpoint = -1
         max_iterations = 3000
