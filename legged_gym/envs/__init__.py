bf2aa338f319341ef749ff5d89197a5ad0bded28
oscar-youngquist <oscar.youngquist@gmail.com>
2026-01-15 14:26:21 -0500

    added support for fine-tuning on challenging dynamics for water-tank like deployment

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -55,6 +55,10 @@ from legged_gym.utils.task_registry import task_registry
 from legged_gym.envs.go1.dynamics_learning.go1_dl_config import GO1DynamicCfg, GO1DynmaicCfgPPO
 from legged_gym.envs.go1.dynamics_learning.go1_dynmaics_learning import LeggedRobotGo1Dynamic
 
+from legged_gym.envs.go1.dynamics_learning_finetuning.go1_dl_config import GO1DynamicFinetuneCfg, GO1DynmaicFinetuneCfgPPO
+from legged_gym.envs.go1.dynamics_learning_finetuning.go1_dynmaics_learning import LeggedRobotGo1DynamicFinetuning
+
+
 task_registry.register( "go2", GO2, GO2Cfg(), GO2CfgPPO())
 task_registry.register( "go2_rough", GO2, GO2RoughCfg(), GO2RoughCfgPPO())
 task_registry.register( "go2_deploy", GO2Deploy, GO2DeployCfg(), GO2DeployCfgPPO())
@@ -63,6 +67,9 @@ task_registry.register( "go2_ts", Go2TS, Go2TSCfg(), Go2TSCfgPPO())
 task_registry.register( "bipedal_walker", BipedalWalker, BipedalWalkerCfg(), BipedalWalkerCfgPPO())
 
 task_registry.register("go1_dynamic", LeggedRobotGo1Dynamic, GO1DynamicCfg(), GO1DynmaicCfgPPO())
+
+task_registry.register("go1_dynamic_ft", LeggedRobotGo1DynamicFinetuning, GO1DynamicFinetuneCfg(), GO1DynmaicFinetuneCfgPPO())
+
 
 from legged_gym.envs.go1_standalone.legged_robot_config import Go1RoughCfg, Go1RoughCfgPPO
 from legged_gym.envs.go1_standalone.legged_robot import LeggedRobot
