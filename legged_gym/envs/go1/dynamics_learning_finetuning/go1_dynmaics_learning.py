7e6c3fa57eebc2059ec2aa9958fd932dc6baf98f
Oscar Youngquist Omen <oscar.youngquist@gmail.com>
2026-01-22 15:06:55 -0500

    mino change before kicking off training run

--- legged_gym/envs/go1/dynamics_learning_finetuning/go1_dynmaics_learning.py
+++ legged_gym/envs/go1/dynamics_learning_finetuning/go1_dynmaics_learning.py
@@ -517,8 +517,8 @@ class LeggedRobotGo1DynamicFinetuning(BaseTask):
                     self._base_com_bias,          # 3
                     self._rand_push_vels[:, :2],  # 2
                     # self._rand_wrench_vels[:, :2], # 2
-                    # self.feedforward_tau_weight, # 1
-                    # self.feedback_tau_weight,    # 1
+                    self.feedforward_tau_weight, # 1
+                    self.feedback_tau_weight,    # 1
                     # heights,                     # 121
                     # mass of water tank
                     # stickness of water in tank
