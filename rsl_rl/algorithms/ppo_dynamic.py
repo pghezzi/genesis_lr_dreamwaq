ee9f096cf1b56ae612ac3828613ecf39323b9e45
Oscar Youngquist <oscar.youngquist@gmail.com>
2026-01-22 15:28:20 -0500

    added support for finetuning on hard dynamics

--- rsl_rl/algorithms/ppo_dynamic.py
+++ rsl_rl/algorithms/ppo_dynamic.py
@@ -311,99 +311,99 @@ class PPODynamic:
                 #  PINN Loss
                 ###
                 # Use the model to generate the "previous" actions (action-pred using previous obs)
-                pinn_loss = 0.0
-                task_align_loss = 0.0
-                task_ratio_loss = 0.0
-                if self.pinn_weight > 0.0:
-                    with torch.no_grad():
-                        if self.use_boot:
-                            self.actor_critic.act(prev_obs_batch, prev_obs_hist_batch)
-                        else:
-                            self.actor_critic.act_bootmask(prev_obs_batch, prev_obs_hist_batch)
-                        prev_actions = torch.cat([self.actor_critic.mean_pos, self.actor_critic.mean_tau], dim=-1)
+                # pinn_loss = 0.0
+                # task_align_loss = 0.0
+                # task_ratio_loss = 0.0
+                # if self.pinn_weight > 0.0:
+                #     with torch.no_grad():
+                #         if self.use_boot:
+                #             self.actor_critic.act(prev_obs_batch, prev_obs_hist_batch)
+                #         else:
+                #             self.actor_critic.act_bootmask(prev_obs_batch, prev_obs_hist_batch)
+                #         prev_actions = torch.cat([self.actor_critic.mean_pos, self.actor_critic.mean_tau], dim=-1)
 
 
-                        pprev_actions = None
-                        if self.use_boot:
-                            self.actor_critic.act(pprev_obs_batch, pprev_obs_hist_batch)
-                        else:
-                            self.actor_critic.act_bootmask(pprev_obs_batch, pprev_obs_hist_batch)
-                        pprev_actions = torch.cat([self.actor_critic.mean_pos, self.actor_critic.mean_tau], dim=-1)
+                #         pprev_actions = None
+                #         if self.use_boot:
+                #             self.actor_critic.act(pprev_obs_batch, pprev_obs_hist_batch)
+                #         else:
+                #             self.actor_critic.act_bootmask(pprev_obs_batch, pprev_obs_hist_batch)
+                #         pprev_actions = torch.cat([self.actor_critic.mean_pos, self.actor_critic.mean_tau], dim=-1)
 
-                        # Process current and previous actions into the action-space
-                        q_des_curr, tau_des_curr   = action_func(current_actions)
-                        q_des_prev, tau_des_prev   = action_func(prev_actions)
-                        q_des_pprev, tau_des_pprev = action_func(pprev_actions)
+                #         # Process current and previous actions into the action-space
+                #         q_des_curr, tau_des_curr   = action_func(current_actions)
+                #         q_des_prev, tau_des_prev   = action_func(prev_actions)
+                #         q_des_pprev, tau_des_pprev = action_func(pprev_actions)
 
-                        # Extract joint pose and velocity data
-                        # Obs - cmd (3), proj_grav (3), ang_vel (3)
-                        q_pos_curr,  q_velo_curr  = obs_batch[:,8:20].detach().clone(),       obs_batch[:,20:32].detach().clone()
-                        q_pos_prev,  q_velo_prev  = prev_obs_batch[:,8:20].detach().clone(),  prev_obs_batch[:,20:32].detach().clone()
-                        q_pos_pprev, q_velo_pprev = pprev_obs_batch[:,8:20].detach().clone(), pprev_obs_batch[:,20:32].detach().clone()
+                #         # Extract joint pose and velocity data
+                #         # Obs - cmd (3), proj_grav (3), ang_vel (3)
+                #         q_pos_curr,  q_velo_curr  = obs_batch[:,8:20].detach().clone(),       obs_batch[:,20:32].detach().clone()
+                #         q_pos_prev,  q_velo_prev  = prev_obs_batch[:,8:20].detach().clone(),  prev_obs_batch[:,20:32].detach().clone()
+                #         q_pos_pprev, q_velo_pprev = pprev_obs_batch[:,8:20].detach().clone(), pprev_obs_batch[:,20:32].detach().clone()
 
-                        q_pos_curr,  q_velo_curr  = (q_pos_curr  + default_pose).float(),  (q_velo_curr / 0.05).float()
-                        q_pos_prev,  q_velo_prev  = (q_pos_prev  + default_pose).float(),  (q_velo_prev / 0.05).float()
-                        q_pos_pprev, q_velo_pprev = (q_pos_pprev + default_pose).float(), (q_velo_pprev / 0.05).float()
+                #         q_pos_curr,  q_velo_curr  = (q_pos_curr  + default_pose).float(),  (q_velo_curr / 0.05).float()
+                #         q_pos_prev,  q_velo_prev  = (q_pos_prev  + default_pose).float(),  (q_velo_prev / 0.05).float()
+                #         q_pos_pprev, q_velo_pprev = (q_pos_pprev + default_pose).float(), (q_velo_pprev / 0.05).float()
 
 
-                        # Calculate feedback torques
-                        # fb_func
-                        pd_tau_curr  = fb_func(q_des_curr,  q_pos_curr,  q_velo_curr)
-                        pd_tau_prev  = fb_func(q_des_prev,  q_pos_prev,  q_velo_prev)
-                        pd_tau_pprev = fb_func(q_des_pprev, q_pos_pprev, q_velo_pprev)
+                #         # Calculate feedback torques
+                #         # fb_func
+                #         pd_tau_curr  = fb_func(q_des_curr,  q_pos_curr,  q_velo_curr)
+                #         pd_tau_prev  = fb_func(q_des_prev,  q_pos_prev,  q_velo_prev)
+                #         pd_tau_pprev = fb_func(q_des_pprev, q_pos_pprev, q_velo_pprev)
 
-                        ###
-                        #   WB-dynamics
-                        ###
-                        # Use 1st order backwards finite differences to approximate models command acceleration
-                        dof_acc = (q_des_curr - 2.0*q_des_prev + q_des_pprev) / np.power(dt,2)
-                        # Create the whole-body acceleration vector
-                        wb_acc = torch.cat([torso_accs_batch, dof_acc], dim=1).float()
-                        # Create the whole-boyd tau vector 
-                        wb_tau = torch.cat([torch.zeros(torso_accs_batch.shape[0], 6).float().to(self.device), tau_des_curr.float()], dim=1).float()
+                #         ###
+                #         #   WB-dynamics
+                #         ###
+                #         # Use 1st order backwards finite differences to approximate models command acceleration
+                #         dof_acc = (q_des_curr - 2.0*q_des_prev + q_des_pprev) / np.power(dt,2)
+                #         # Create the whole-body acceleration vector
+                #         wb_acc = torch.cat([torso_accs_batch, dof_acc], dim=1).float()
+                #         # Create the whole-boyd tau vector 
+                #         wb_tau = torch.cat([torch.zeros(torso_accs_batch.shape[0], 6).float().to(self.device), tau_des_curr.float()], dim=1).float()
 
-                        # Calculate the models wb-dynamics
-                        model_wb_dynamics = torch.bmm(mass_mat_batch.float(), wb_acc.unsqueeze(-1)).squeeze(-1) + bias_vec_batch.float()
+                #         # Calculate the models wb-dynamics
+                #         model_wb_dynamics = torch.bmm(mass_mat_batch.float(), wb_acc.unsqueeze(-1)).squeeze(-1) + bias_vec_batch.float()
 
-                        error = model_wb_dynamics[:,6:] - gt_forces_batch[:,6:] - wb_tau[:,6:]
+                #         error = model_wb_dynamics[:,6:] - gt_forces_batch[:,6:] - wb_tau[:,6:]
 
-                        # softly weight by contact
-                        # Apply soft (to make this a continuous reward signal) contact weighting to avoid over-penalizing for leg movement
-                        contact_magnitude = torch.clamp(gt_forces_batch[:,6:], min=0.0)
-                        contact_max = torch.max(contact_magnitude, dim=1, keepdim=True)[0]
-                        contact_weight = contact_magnitude / (contact_max + 1e-8)
-                        error *= contact_weight
+                #         # softly weight by contact
+                #         # Apply soft (to make this a continuous reward signal) contact weighting to avoid over-penalizing for leg movement
+                #         contact_magnitude = torch.clamp(gt_forces_batch[:,6:], min=0.0)
+                #         contact_max = torch.max(contact_magnitude, dim=1, keepdim=True)[0]
+                #         contact_weight = contact_magnitude / (contact_max + 1e-8)
+                #         error *= contact_weight
 
-                        # Make the error relative, so that it is less senesitive to scale
-                        rel_error = torch.norm(error, dim=1) / (1e-8 + torch.norm(wb_tau[:,6:].detach().clone(), dim=1) + torch.norm(gt_forces_batch[:,6:], dim=1))
+                #         # Make the error relative, so that it is less senesitive to scale
+                #         rel_error = torch.norm(error, dim=1) / (1e-8 + torch.norm(wb_tau[:,6:].detach().clone(), dim=1) + torch.norm(gt_forces_batch[:,6:], dim=1))
 
-                        # Calculate the whole-body PINN loss
-                        pinn_loss = torch.mean(rel_error)
+                #         # Calculate the whole-body PINN loss
+                #         pinn_loss = torch.mean(rel_error)
 
-                        # Calcualte values used by the next two losses
-                        ff_power_curr, pd_power_curr   = tau_des_curr*q_velo_curr,   pd_tau_curr*q_velo_curr
-                        ff_power_prev, pd_power_prev   = tau_des_prev*q_velo_prev,   pd_tau_prev*q_velo_prev
-                        ff_power_pprev, pd_power_pprev = tau_des_pprev*q_velo_pprev, pd_tau_pprev*q_velo_pprev
+                #         # Calcualte values used by the next two losses
+                #         ff_power_curr, pd_power_curr   = tau_des_curr*q_velo_curr,   pd_tau_curr*q_velo_curr
+                #         ff_power_prev, pd_power_prev   = tau_des_prev*q_velo_prev,   pd_tau_prev*q_velo_prev
+                #         ff_power_pprev, pd_power_pprev = tau_des_pprev*q_velo_pprev, pd_tau_pprev*q_velo_pprev
 
-                        ff_power_val_curr, pd_power_val_curr   = torch.abs(torch.sum(ff_power_curr)), torch.abs(torch.sum(pd_power_curr))
-                        ff_power_val_prev, pd_power_val_prev   = torch.abs(torch.sum(ff_power_prev)), torch.abs(torch.sum(pd_power_prev))
-                        ff_power_val_pprev, pd_power_val_pprev = torch.abs(torch.sum(ff_power_pprev)), torch.abs(torch.sum(pd_power_pprev))
+                #         ff_power_val_curr, pd_power_val_curr   = torch.abs(torch.sum(ff_power_curr)), torch.abs(torch.sum(pd_power_curr))
+                #         ff_power_val_prev, pd_power_val_prev   = torch.abs(torch.sum(ff_power_prev)), torch.abs(torch.sum(pd_power_prev))
+                #         ff_power_val_pprev, pd_power_val_pprev = torch.abs(torch.sum(ff_power_pprev)), torch.abs(torch.sum(pd_power_pprev))
 
-                        # Calculate a minimum effort gate
-                        #    detach the gate values from the comp-graph, do not need gradients flowing through this
-                        curr_effort_gate  = F.tanh(ff_power_val_curr.detach().clone() + pd_power_val_curr.detach().clone())
-                        prev_effort_gate  = F.tanh(ff_power_val_prev.detach().clone() + pd_power_val_prev.detach().clone())
-                        pprev_effort_gate = F.tanh(ff_power_val_pprev.detach().clone() + pd_power_val_pprev.detach().clone())
+                #         # Calculate a minimum effort gate
+                #         #    detach the gate values from the comp-graph, do not need gradients flowing through this
+                #         curr_effort_gate  = F.tanh(ff_power_val_curr.detach().clone() + pd_power_val_curr.detach().clone())
+                #         prev_effort_gate  = F.tanh(ff_power_val_prev.detach().clone() + pd_power_val_prev.detach().clone())
+                #         pprev_effort_gate = F.tanh(ff_power_val_pprev.detach().clone() + pd_power_val_pprev.detach().clone())
 
-                        # Now create losses that seek to align power generation
-                        #    using cosine similarity to enforce power expenditures that don't conflict 
-                        task_alignment_curr  = torch.clamp(-F.cosine_similarity(ff_power_curr,  pd_power_curr),  min=0.0)
-                        task_alignment_prev  = torch.clamp(-F.cosine_similarity(ff_power_prev,  pd_power_prev),  min=0.0)
-                        task_alignment_pprev = torch.clamp(-F.cosine_similarity(ff_power_pprev, pd_power_pprev), min=0.0)
+                #         # Now create losses that seek to align power generation
+                #         #    using cosine similarity to enforce power expenditures that don't conflict 
+                #         task_alignment_curr  = torch.clamp(-F.cosine_similarity(ff_power_curr,  pd_power_curr),  min=0.0)
+                #         task_alignment_prev  = torch.clamp(-F.cosine_similarity(ff_power_prev,  pd_power_prev),  min=0.0)
+                #         task_alignment_pprev = torch.clamp(-F.cosine_similarity(ff_power_pprev, pd_power_pprev), min=0.0)
 
-                        task_align_loss = torch.mean(torch.square(curr_effort_gate*task_alignment_curr + \
-                                                                prev_effort_gate*task_alignment_prev + \
-                                                                pprev_effort_gate*task_alignment_pprev))
+                #         task_align_loss = torch.mean(torch.square(curr_effort_gate*task_alignment_curr + \
+                #                                                 prev_effort_gate*task_alignment_prev + \
+                #                                                 pprev_effort_gate*task_alignment_pprev))
                         
                         # # Finally, create a loss based on achiving a desired ratio of mechanical power.
                         # tagret_ratio = 1.8
@@ -502,10 +502,10 @@ class PPODynamic:
                 mean_recon_loss += recon_error.item()
                 mean_kld_loss += kl_div.item()
                 mean_decoder_loss += dec_loss.item()
-                if self.pinn_weight > 0.0:
-                    mean_pinn_loss += pinn_loss.item() + task_align_loss.item() # + task_ratio_loss.item()
-                else:
-                    mean_pinn_loss += pinn_loss + task_align_loss # + task_ratio_loss
+                # if self.pinn_weight > 0.0:
+                #     mean_pinn_loss += pinn_loss.item() + task_align_loss.item() # + task_ratio_loss.item()
+                # else:
+                #     mean_pinn_loss += pinn_loss + task_align_loss # + task_ratio_loss
                     
         if use_pinn and itr > self.pinn_init:
             self.num_pinn_updates += 1
